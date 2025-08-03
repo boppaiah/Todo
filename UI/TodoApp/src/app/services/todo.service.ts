dbd1e80bd51717cbcbb5a85f2482c00bc675bcc6
boppaiah <boppaiah38@yahoo.in>
2025-08-03 21:14:56 +1000

    update to angular 18

--- UI/TodoApp/src/app/services/todo.service.ts
+++ UI/TodoApp/src/app/services/todo.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,HttpParams } from "@angular/common/http"
+import { HttpClient, HttpParams } from "@angular/common/http"
 import { environment } from '../../environments/environment';
 import { Todo } from '../models/todo';
 import { NgForm } from '@angular/forms';
